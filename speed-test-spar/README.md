cefefa732471a165080f041eb7c7fc28526e03a7
maryanne anyango <maryanneanyango98@gmail.com>
2022-05-18 15:18:00 +0300

    m

---
+++ speed-test-spar/README.md
@@ -0,0 +1,49 @@
+Delani Studio
+ ## By Maryanne Anyango
+ ## Description
+ <p>This project is a web application for Speed-Test-Spar,,shows services offered to students,,questions to answer and learn</p>
+[Go Back to the top](#Speed-Test-Spar)
+ ###  Requirements
+ * Access to  a computer or any other garget
+ * Access to internet
+ ### Installation Process
+ ****
+* Clone to this repo : git clone 
+* Unzip the downloaded files in a folder of choice.
+* Open the index file from the zipped file with any browser.
+ ****
+ [Go Back to the top](#speed-test-spar)
+### Live Link
+- Click this link to view the live application 
+### Technology  Used
+* HTML - which was used to develope the structure off the pages.
+* CSS - which was used to style the User Interface.
+* javascript      
+## Known Bugs
+* Mailchimp API does not function correctly as expected.
+## Licence
+MIT License
+Copyright (c) [2019] [Albert Byrone]
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+[Go Back to the top](#delani-studio)
+## Authors Info
+Slack Profile - [Albert Byrone](https://app.slack.com/client/T077KKCG6/GLRQR61NW/user_profile/UKXCHMCNP?cdn_fallback=1)
+Linked - [Albert Byrone](https://www.linkedin.com/in/albert-byrone-664811144/)
+[Go Back to the top](#delani-studio)
+ 
+@channel Sample README file
+Show
---
+++ speed-test-spar/javascript/results.js
@@ -0,0 +1,128 @@
+percentage = 0;
+remark = " ";
+
+function submitToJs() {
+
+    marks = 0;
+
+    // The checked property sets or returns the checked state of a checkbox.
+
+    // check inputs if selected
+
+    // question 1
+    if (document.getElementById('t1').checked ||
+        document.getElementById('af1').checked ||
+        document.getElementById('bf1').checked ||
+        document.getElementById('cf1').checked) {
+
+        var q1 = document.getElementById('t1').checked;
+        if (q1 == true) {
+            marks = marks + 1;
+        } else {
+            marks = marks + 0;
+        }
+    } else {
+        alert("Please answer Question 1 ");
+        return false;
+    }
+
+    // question2
+    if (document.getElementById('t2').checked ||
+        document.getElementById('af2').checked ||
+        document.getElementById('bf2').checked ||
+        document.getElementById('cf2').checked) {
+
+        var q2 = document.getElementById('t2').checked;
+
+        if (q2 == true) {
+            marks = marks + 1;
+        } else {
+            marks = marks + 0;
+        }
+
+    } else {
+        alert("Please answer question 2 ");
+        return false;
+    }
+
+    // question3
+    if (document.getElementById('t3').checked ||
+        document.getElementById('af3').checked ||
+        document.getElementById('bf3').checked ||
+        document.getElementById('cf3').checked) {
+
+        var q3 = document.getElementById('t3').checked;
+
+        if (q3 == true) {
+            marks = marks + 1;
+        } else {
+            marks = marks + 0;
+        }
+
+    } else {
+        alert("Please answer Question 3 ");
+        return false;
+    }
+
+    // question4
+    if (document.getElementById('t4').checked ||
+        document.getElementById('af4').checked ||
+        document.getElementById('bf4').checked ||
+        document.getElementById('cf4').checked) {
+
+        var q4 = document.getElementById('t4').checked;
+
+        if (q4 == true) {
+            marks = marks + 1;
+        } else {
+            marks = marks + 0;
+        }
+
+    } else {
+        alert("Please answer Question 4 ");
+        return false;
+    }
+
+    // question5
+    if (document.getElementById('t5').checked ||
+        document.getElementById('af5').checked ||
+        document.getElementById('bf5').checked ||
+        document.getElementById('cf5').checked) {
+
+        var q5 = document.getElementById('t5').checked;
+
+        if (q5 == true) {
+            marks = marks + 1;
+        } else {
+            marks = marks + 0;
+        }
+
+    } else {
+        alert("Please answer Question 5 ");
+        return false;
+    }
+
+    var percentage = (marks / 5) * 100;
+
+
+    if (percentage >= 80 && percentage <= 100) {
+        var remark = "Excellently passes. ";
+    } else if (percentage >= 50 && percentage < 80) {
+        var remark = "Fairly passed. ";
+    } else {
+        var remark = "Retake the test. ";
+    }
+
+
+    var p1_target = document.getElementById("p1");
+    var p2_target = document.getElementById("p2");
+
+    p1_target.innerHTML += "<p>You have scored : " + percentage + "%</p>";
+    p2_target.innerHTML += " <p> Comment: " + remark + " </p>";
+
+    document.getElementsByClassName('bigContainer')[4].style.display = "none";
+    // document.getElementsByClassName('submitClass')[0].style.display = "none";
+    document.getElementsByClassName('results_display')[0].style.display = "block";
+
+    return false;
+}
